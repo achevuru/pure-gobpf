498c05235dbaf47ee6db40ceabcf89d7328e7a22
Apurup Chevuru <achevuru@amazon.com>
2023-01-08 13:30:34 -0800

    mnt bpffs

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/achevuru/pure-gobpf
 go 1.19
 
 require (
+	github.com/jayanthvn/pure-gobpf v0.0.0-20230108071427-2d96eecf2a3d // indirect
 	github.com/sirupsen/logrus v1.9.0 // indirect
 	github.com/vishvananda/netlink v1.1.0 // indirect
 	github.com/vishvananda/netns v0.0.0-20191106174202-0a2b9b5464df // indirect
--- pkg/ebpf/loader.go
+++ pkg/ebpf/loader.go
@@ -19,6 +19,7 @@ import (
 	"os"
 	"path/filepath"
 	"runtime"
+	"syscall"
 	"unsafe"
 
 	"github.com/jayanthvn/pure-gobpf/pkg/logger"
@@ -137,6 +138,16 @@ func (m *BpfMapData) CreateMap() (int, error) {
 	return int(ret), nil
 }
 
+func mount_bpf_fs() error {
+	var log = logger.Get()
+	log.Infof("Let's mount BPF FS")
+	err := syscall.Mount("bpf", "/sys/fs/bpf", "bpf", 0, "mode=0700")
+	if err != nil{
+		log.Errorf("error mounting bpffs: %v", err)
+	}
+	return err
+}
+
 func (m *BpfMapData) PinMap(mapFD int, pinPath string) error {
 	var log = logger.Get()
 	if m.Def.Pinning == PIN_NONE {
@@ -170,7 +181,13 @@ func (m *BpfMapData) PinMap(mapFD int, pinPath string) error {
 func PinProg(progFD int, pinPath string) error {
 	var log = logger.Get()
 
-	err := os.MkdirAll(filepath.Dir(pinPath), 0755)
+	err := mount_bpf_fs()
+	if err != nil{
+		log.Errorf("error mounting bpffs: %v", err)
+		return err
+	}
+
+	err = os.MkdirAll(filepath.Dir(pinPath), 0755)
 	if err != nil {
 		log.Infof("error creating directory %q: %v", filepath.Dir(pinPath), err)
 		return fmt.Errorf("error creating directory %q: %v", filepath.Dir(pinPath), err)
